cbfdd4dffb35585e92a19eb84aa86cc948cc0049
Limon4ikVf <Limon4ikvf@gmail.com>
2026-02-07 15:03:04 +0200

    Add core entities and enums

---
+++ Cinema.Core/Entities/Session.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Sockets;
+
+namespace Cinema.Core.Entities
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; } = null!;
+
+        public int HallId { get; set; }
+        public Hall Hall { get; set; } = null!;
+
+        public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+    }
+}
---
+++ Cinema.Core/Enums/TicketStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Cinema.Core.Enums
+{
+    public enum TicketStatus
+    {
+        Booked = 1,
+        Paid = 2,
+        Cancelled = 3
+    }
+}
