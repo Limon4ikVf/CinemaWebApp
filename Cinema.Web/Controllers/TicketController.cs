b1f7c72cd00c88f25bbc669cb6097b224bedd59b
Limon4ikVf <Limon4ikvf@gmail.com>
2026-02-12 18:37:42 +0200

    New page Ticket

---
+++ Cinema.Web/Controllers/TicketController.cs
@@ -0,0 +1,32 @@
+﻿using Cinema.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Web.Controllers
+{
+    public class TicketController : Controller
+    {
+        private readonly CinemaDbContext _context;
+
+        public TicketController(CinemaDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int sessionId)
+        {
+            var session = await _context.Sessions
+                .Include(s => s.Movie)
+                .Include(s => s.Hall)
+                .FirstOrDefaultAsync(s => s.Id == sessionId);
+
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return View(session);
+        }
+    }
+}
