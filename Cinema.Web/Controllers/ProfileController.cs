6b93a2b521bc9b5d8a1bc3388f47a90606978a5d
Limon4ikVf <Limon4ikvf@gmail.com>
2026-02-13 01:20:16 +0200

    Implement user profile page

---
+++ Cinema.Web/Controllers/ProfileController.cs
@@ -0,0 +1,33 @@
+﻿using Cinema.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly CinemaDbContext _context;
+
+        public ProfileController(CinemaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // тимчасово
+            int currentUserId = 1;
+
+            var tickets = await _context.Tickets
+                .Include(t => t.Session)
+                    .ThenInclude(s => s.Movie)
+                .Include(t => t.Session)
+                    .ThenInclude(s => s.Hall)
+                .Where(t => t.UserId == currentUserId)
+                .OrderByDescending(t => t.PurchaseDate)
+                .ToListAsync();
+
+            return View(tickets);
+        }
+    }
+}
