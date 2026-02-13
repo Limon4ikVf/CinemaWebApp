f99ff20db9aad78ffeb3ce2a6dd431f28f654c1d
Limon4ikVf <Limon4ikvf@gmail.com>
2026-02-13 12:45:42 +0200

    Implement edit and  delete sessions

--- Cinema.Web/Controllers/AdminSessionController.cs
+++ Cinema.Web/Controllers/AdminSessionController.cs
@@ -58,5 +58,52 @@ namespace Cinema.Web.Controllers
             ViewBag.Halls = new SelectList(await _context.Halls.ToListAsync(), "Id", "Name", session.HallId);
             return View(session);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var session = await _context.Sessions.FindAsync(id);
+            if (session == null) return NotFound();
+
+            ViewBag.Movies = new SelectList(await _context.Movies.ToListAsync(), "Id", "Title", session.MovieId);
+            ViewBag.Halls = new SelectList(await _context.Halls.ToListAsync(), "Id", "Name", session.HallId);
+
+            return View(session);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Session session)
+        {
+            if (id != session.Id) return NotFound();
+
+            ModelState.Remove("Movie");
+            ModelState.Remove("Hall");
+            ModelState.Remove("Tickets");
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(session);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Movies = new SelectList(await _context.Movies.ToListAsync(), "Id", "Title", session.MovieId);
+            ViewBag.Halls = new SelectList(await _context.Halls.ToListAsync(), "Id", "Name", session.HallId);
+            return View(session);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var session = await _context.Sessions.FindAsync(id);
+            if (session != null)
+            {
+                _context.Sessions.Remove(session);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
