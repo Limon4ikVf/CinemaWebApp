da66fcb31999be8d1e1d1061226c1ff342bd6cea
Limon4ikVf <Limon4ikvf@gmail.com>
2026-02-13 10:37:08 +0200

    implement admin panel

---
+++ Cinema.Web/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Cinema.Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly CinemaDbContext _context;
+
+        public AdminController(CinemaDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _context.Movies
+                .OrderByDescending(m => m.Id)
+                .ToListAsync();
+
+            return View(movies);
+        }
+    }
+}
