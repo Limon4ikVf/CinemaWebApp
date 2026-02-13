3ec5c853c72541e4450e7acb49c719790c050cdb
Limon4ikVf <Limon4ikvf@gmail.com>
2026-02-13 11:30:17 +0200

    Add movie creation

--- Cinema.Web/Controllers/AdminController.cs
+++ Cinema.Web/Controllers/AdminController.cs
@@ -23,5 +23,38 @@ namespace Cinema.Web.Controllers
 
             return View(movies);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Genres = await _context.Genres.ToListAsync();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Cinema.Core.Entities.Movie movie, int[] selectedGenreIds)
+        {
+            if (ModelState.IsValid)
+            {
+                if (selectedGenreIds != null && selectedGenreIds.Any())
+                {
+                    var selectedGenres = await _context.Genres
+                        .Where(g => selectedGenreIds.Contains(g.Id))
+                        .ToListAsync();
+
+                    movie.Genres = selectedGenres;
+                }
+
+                _context.Movies.Add(movie);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Genres = await _context.Genres.ToListAsync();
+            return View(movie);
+        }
+
     }
 }
