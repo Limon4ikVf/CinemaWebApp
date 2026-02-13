574ab1f15dff2a4cd808856686c3d64052811353
Limon4ikVf <Limon4ikvf@gmail.com>
2026-02-13 11:49:13 +0200

    Implement edit and delet movies

--- Cinema.Web/Controllers/AdminController.cs
+++ Cinema.Web/Controllers/AdminController.cs
@@ -56,5 +56,69 @@ namespace Cinema.Web.Controllers
             return View(movie);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var movie = await _context.Movies
+                .Include(m => m.Genres)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (movie == null) return NotFound();
+
+            ViewBag.Genres = await _context.Genres.ToListAsync();
+            return View(movie);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Cinema.Core.Entities.Movie updatedMovie, int[] selectedGenreIds)
+        {
+            if (id != updatedMovie.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                var existingMovie = await _context.Movies
+                    .Include(m => m.Genres)
+                    .FirstOrDefaultAsync(m => m.Id == id);
+
+                if (existingMovie == null) return NotFound();
+
+                existingMovie.Title = updatedMovie.Title;
+                existingMovie.Description = updatedMovie.Description;
+                existingMovie.DurationMinutes = updatedMovie.DurationMinutes;
+                existingMovie.ReleaseDate = updatedMovie.ReleaseDate;
+                existingMovie.PosterUrl = updatedMovie.PosterUrl;
+                existingMovie.Director = updatedMovie.Director;
+                existingMovie.TrailerUrl = updatedMovie.TrailerUrl;
+
+                existingMovie.Genres.Clear();
+                if (selectedGenreIds != null && selectedGenreIds.Any())
+                {
+                    existingMovie.Genres = await _context.Genres
+                        .Where(g => selectedGenreIds.Contains(g.Id))
+                        .ToListAsync();
+                }
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.Genres = await _context.Genres.ToListAsync();
+            return View(updatedMovie);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Movies.Remove(movie);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
